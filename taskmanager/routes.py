206233451f409df5ce5b984a5c5c7b5e86c62f4b
Martin Bridgman <martin.bridgman@gmail.com>
2022-03-01 16:57:58 +0000

    Adding categories complete and debugged

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -13,7 +13,11 @@ def categories():
     return render_template("categories.html")
 
 
-@app.route("/add_category")
+@app.route("/add_category", methods=["GET", "POST"])
 def add_category():
-   
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
