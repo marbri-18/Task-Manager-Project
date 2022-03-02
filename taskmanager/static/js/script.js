f4b9a0ccc49c8c4232d0303d45ed6a2ab7d62e96
Martin Bridgman <martin.bridgman@gmail.com>
2022-03-02 23:56:57 +0000

    Adding tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
+  
+
+  // Date Picker
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+  
+
+  // Category Id select box
+  
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
+
+});
