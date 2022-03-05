e3998405a4e96504fb9a837a47284a3e27ee7861
Martin Bridgman <martin.bridgman@gmail.com>
2022-03-05 11:26:19 +0000

    Viewing Tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -17,5 +17,9 @@ document.addEventListener('DOMContentLoaded', function() {
     let selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
 
+  // Collapsible initialisation
+  let collapsibles = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collapsibles);
+
 
 });
