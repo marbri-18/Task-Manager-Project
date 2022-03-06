58faf62d59efb650dc9291aaf95bb6887817e4d6
Martin Bridgman <martin.bridgman@gmail.com>
2022-03-06 13:24:10 +0000

    Modifify init file

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -75,7 +75,7 @@ def edit_task(task_id):
 
 @app.route("/delete_task/<int:task_id>")
 def delete_task(task_id):
-    task = Task.query.get_or_404(category_id)
+    task = Task.query.get_or_404(task_id)
     db.session.delete(task)
     db.session.commit()
     return redirect(url_for("home"))
